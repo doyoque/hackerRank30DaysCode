d7eea99438e831019f173d480753a345cb471cf1
doyoque <abiyoga1335@gmail.com>
2020-03-11 22:46:17 +0700

    rename the source, data types assessment

---
+++ dataTypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int i = 4;
+	double d = 4.0;
+	string s = "HackerRank ";
+
+	//Declare second integer, double, and string variables.
+	//Read and save an integer, double, and string to your variables.
+	//Note: if you have trouble reading the entire string, please go back and review the tutorial closely.
+	//print the sum of both integer variables on a new line.
+	//print the sum of the double variables on a new line.
+	//concatenate and print the string variables on a new line
+	//the 's' variable above should be printed first.
+	
+	return 0;	
+}
