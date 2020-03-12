2a5c14027986fa519737abd10b29567a7ebc8fbb
doyoque <abiyoga1335@gmail.com>
2020-03-12 11:04:52 +0700

    solution for data types 30 days of code

--- dataTypes.cpp
+++ dataTypes.cpp
@@ -8,11 +8,31 @@ int main() {
 	string s = "HackerRank ";
 
 	//Declare second integer, double, and string variables.
+	int j;
+	double b;
+	string n;
+
 	//Read and save an integer, double, and string to your variables.
+	string tmp;
+	
+	getline(cin, tmp);
+	j = stoi(tmp);
+
+	getline(cin, tmp);
+	b = stod(tmp);
+
+	getline(cin, n);
+
 	//Note: if you have trouble reading the entire string, please go back and review the tutorial closely.
 	//print the sum of both integer variables on a new line.
+	printf("%i\n", i + j);
+
 	//print the sum of the double variables on a new line.
+	printf("%.1f\n", d + b);
+
 	//concatenate and print the string variables on a new line
+	cout << s + n << endl;
+
 	//the 's' variable above should be printed first.
 	
 	return 0;	
